62ae745cf9c3989bca995ff78eefa2a5bc8a7932
OttoRobotto <alexander.ottenhoff@gmail.com>
2014-11-05 16:11:48 +0100

    THREE wasn't being exported

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "Three.js core library + several useful extensions from the examples folder.",
-	version: "0.69.0",
+	version: "0.69.1",
 	name: "limemakers:three",
 	git: "https://github.com/limemakers/meteor-three.git",
 });
