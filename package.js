eb6034308c515a9e1cba813fe34a6e3284d2e9e7
Pavel Khlopin <nyao7765@gmail.com>
2015-04-13 20:32:34 +0300

    bump version number to 0.71.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "Three.js core library + several useful extensions from the examples folder.",
-	version: "0.69.5",
+	version: "0.71.0",
 	name: "limemakers:three",
 	git: "https://github.com/limemakers/meteor-three.git",
 });
